dd9caf28eeeaae7a1f2ac8ffb41dc95de2443459
steve <steve.nguyen@linksinternational.com>
2023-01-03 23:00:43 +0700

    Serve static file

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,6 +10,8 @@ import { RolesGuard } from './auth/roles.guard';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { BooksModule } from './books/books.module';
 import { Books } from './books/books.entity';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 console.log("== __dirname ", __dirname);
 
@@ -33,6 +35,10 @@ console.log("== __dirname ", __dirname);
     MongooseModule.forRoot(process.env.DATABASE_HOST), 
     RestaurantModule, 
     AuthModule, 
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'swagger-static'),
+      serveRoot: process.env.NODE_ENV === 'development' ? '/' : '/swagger',
+    })
     // BooksModule,
   ],
   controllers: [AppController],
--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,9 @@ import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 import { json, urlencoded } from 'express';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
-
+import { resolve } from 'path';
+import { writeFileSync, createWriteStream } from 'fs';
+import { get } from 'http';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -12,15 +14,50 @@ async function bootstrap() {
   app.use(json({ limit: '50mb' }));
   app.use(urlencoded({ extended: true, limit: '50mb' }));
   const config = new DocumentBuilder()
-    .setTitle('Cats example')
+    .setTitle('API crud basic')
     .setDescription('The cats API description')
     .setVersion('1.0')
     .addTag('cats')
     .build();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+  SwaggerModule.setup('swagger', app, document);
 
   await app.listen(process.env.PORT, '0.0.0.0');
+  const serverUrl = process.env.serverUrl || 'http://localhost:3000';
+  // write swagger ui files
+  get(
+    `${serverUrl}/swagger/swagger-ui-bundle.js`, function 
+    (response) {
+      response.pipe(createWriteStream('swagger-static/swagger-ui-bundle.js'));
+      console.log(
+  `Swagger UI bundle file written to: '/swagger-static/swagger-ui-bundle.js'`,
+);
+  });
+
+  get(`${serverUrl}/swagger/swagger-ui-init.js`, function (response) {
+    response.pipe(createWriteStream('swagger-static/swagger-ui-init.js'));
+    console.log(
+  `Swagger UI init file written to: '/swagger-static/swagger-ui-init.js'`,
+);
+  });
+
+  get(
+`${serverUrl}/swagger/swagger-ui-standalone-preset.js`,
+function (response) {
+    response.pipe(
+    createWriteStream('swagger-static/swagger-ui-standalone-preset.js'),
+  );
+    console.log(
+    `Swagger UI standalone preset file written to: '/swagger-static/swagger-ui-standalone-preset.js'`,
+  );
+  });
+
+  get(`${serverUrl}/swagger/swagger-ui.css`, function (response) {
+    response.pipe(createWriteStream('swagger-static/swagger-ui.css'));
+    console.log(
+  `Swagger UI css file written to: '/swagger-static/swagger-ui.css'`,
+);
+  });
 }
 bootstrap();
 
